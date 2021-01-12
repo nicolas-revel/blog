6efc7699081792e2ade13e8fa4e92e19416d38da
Laprevote Emma <emma.laprevote@laplateforme.io>
2021-01-12 09:14:57 +0100

    première structure classe article model/controller

--- app/controllers/Article.php
+++ app/controllers/Article.php
@@ -6,5 +6,72 @@ namespace blog\app\controllers;
 
 class Article
 {
+    /**
+     * Méthode qui traite le formulaire de création d'article puis insère dans la BDD
+     * @return void
+     */
+    public function insertArticle (): void {
+        // Traitement du formulaire 'creer_article'
+        //Appel de la fonction ->findCategorie () 'blog\app\models', pour récupérer l'id de la categorie
+        //Appel de la fonction ->insertArticle($categorie, $article, $id_utilisateur, $id_categorie) 'blog\app\models'
+        //Redirection
+    }
+
+    /**
+     * Méthode qui traite de formulaire de modification d'article puis remplace les nouvelles informations dans la BDD
+     * @param int $id
+     * @return void
+     */
+    public function updateArticle (int $id): void {
+        // Traitement des $_GET
+        //Traitement du formulaire 'creer_article'
+        //Appel de la fonction ->updateArticle($id) 'blog\app\models'
+        //Redirection
+    }
+
+    /**
+     * Méthode qui permet de supprimer un article par rapport bouton/lien
+     * @return void
+     */
+    public function deletArticle (): void {
+        //Traitement des $_GET
+        // Appel de la fonction ->findCategorie () pour afficher le nom de la catégorie
+        //Appel de la fonction ->findArticle($id_utilisateur) 'blog\app\models' pour récupérer l'id de l'article
+        //Appel de la fonction ->deletArticle($id) 'blog\app\models'
+        //Redirection
+    }
+
+    /**
+     * Méthode qui permet d'afficher les 3 premiers articles sur la page d'accueil
+     */
+    public function showAccueil () {
+        // Appel de la fonction ->findCategorie () pour afficher le nom de la catégorie
+        //Appel de la fonction ->getThirdArticle () 'blog\app\models'
+        // Redirection avec Renderer ?
+    }
+
+    /**
+     * Méthode qui permet d'afficher l'ensemble des article (max 5) du plus récent au plus ancien + pagination
+     */
+    public function showArticles () {
+        //Traitement de $_GET pour pagination
+        // Appel de la fonction ->findCategorie () pour afficher le nom de la catégorie
+        //Appel de la fonction ->getFiveArticle () 'blog\app\models'
+        // Redirection avec Renderer ?
+
+    }
+
+    public function showArticle () {
+        //Traitement des $_GET
+        //Appel de la fonction ->findArticle($id_utilisateur) 'blog\app\models' pour récupérer l'id de l'article
+        //$article = $this->model->find($id);
+        //Récupération des commentaires liés à l'article
+        // Redirection avec Renderer ?
+
+    }
+
+
+
+
 
 }
--- app/models/Article.php
+++ app/models/Article.php
@@ -6,5 +6,101 @@ namespace blog\app\models;
 
 class Article
 {
+    private $id;
+    public $article;
+    public $id_categorie;
+    public $date;
+
+    /**
+     * Methode qui permet d'inserer un article dans la base de donnée
+     * @param int $article
+     * @return void
+     */
+    public function insertArticle ($categorie, $article, $id_utilisateur, $id_categorie): void {
+        //Appel méthode ->getBdd()
+        //Requête SQL INSERT INTO table 'article'
+    }
+
+    /**
+     * Méthode qui permet de modifier un article dans la base de donnée
+     * @param int $id
+     * @return void
+     */
+    public function updateArticle (int $id): void {
+        //Appel méthode ->getBdd()
+        //Requête SQL UPDATE table 'article'
+    }
+
+    /**
+     * Méthode qui permet la suppresion d'un article dans la base de donnée
+     * @param $id
+     * @return void
+     */
+    public function deleteArticle ($id): void {
+        //Appel méthode ->getBdd()
+        //Requête SQL DELETE table 'article'
+    }
+
+    /**
+     * Méthode qui permet de récupérer toute les informations et qui seront "stockées" dans les attributs
+     * @param $id_utilisateur
+     * @return array
+     */
+    public function findArticle ($id_utilisateur): array {
+        //Appel méthode ->getBdd()
+        //Requête SQL SELECT * table 'article'
+        //Valeurs attribuées aux attributs (PDO::FECTH_OBJ)
+    }
+
+    /**
+     * Méthode qui permet de récupérer un article par rapport à son id
+     * @param $id
+     * @return array
+     */
+    public function find ($id): array {
+        //Appel méthode ->getBdd()
+        //Requête SQL SELECT * table 'article'
+    }
+
+    /**
+     * Méthode qui permet de récupérer l'id et le nom des catégories
+     * @return array
+     */
+    public function findCategorie (): array {
+        //Appel méthode ->getBdd()
+        //Requête SQL SELECT id, nom table 'categorie'
+    }
+
+    /**
+     * Méthode qui permet de récupérer les 3 derniers articles ou les 5 derniers du plus récent au plus ancien
+     * @return array
+     */
+    public function getArticle (?int $line = ""): array {
+        //Appel méthode ->getBdd()
+        //Requête = SQL SELECT * FROM LIMIT $line ORDER BY DESC
+
+        if($line){
+            $requête .= " LIMIT " . $line;
+        }
+
+        $resultats = $this->pdo->query($requête);
+        // On fouille le résultat pour en extraire les données réelles
+        $articles = $resultats->fetchAll();
+
+    }
+
+    public function getBdd() {
+
+        $pdo = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root', [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+
+        return $pdo;
+    }
+    }
+
+
+
 
 }
