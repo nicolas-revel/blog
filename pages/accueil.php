7f8dc19dfc4da9f8d627ba0a7030904d7a014b0b
Laprevote Emma <emma.laprevote@laplateforme.io>
2021-01-20 16:39:59 +0100

    css accueil en cours

--- pages/accueil.php
+++ pages/accueil.php
@@ -8,6 +8,21 @@ $show = new blog\app\views\Article();
 <?php require_once('../config/header.php'); ?>
 
 <main id="homeArticles">
+
+    <section id="present_blog">
+        <div id="avatars">
+            <img id="avatarNico" src="../images/avatarnico.jpg">
+            <img id="avatarEmma" src="../images/avataremma.jpg">
+        </div>
+        <div id="elementColumn">
+            <h2 id="title_blogout">Hello World!</h2>
+            <p id="presentBlog">bLogOut est un petit blog créé par Nicolas REVEL et Emma LAPREVOTE,
+                tout deux étudiants au sein de l'école La Plateforme et passionnés par le développement web.
+            Ce blog est une vue généralisé de tout ce qui se passe dans l'univers du web, sur nos expériences tout au long de l'apprentissage.
+            </p>
+        </div>
+    </section>
+
 <div class="card_articles"><?= $show->showArticleAccueil(); ?></div>
 
 <a href="articles.php">Articles</a>
