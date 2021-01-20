f45625ff57be0955c3bf6112c29608e54181d222
Laprevote Emma <emma.laprevote@laplateforme.io>
2021-01-20 21:17:07 +0100

    css footer terminé

--- pages/accueil.php
+++ pages/accueil.php
@@ -15,18 +15,25 @@ $show = new blog\app\views\Article();
             <img id="avatarEmma" src="../images/avataremma.jpg">
         </div>
         <div id="elementColumn">
-            <h2 id="title_blogout">Hello World!</h2>
-            <p id="presentBlog">bLogOut est un petit blog créé par Nicolas REVEL et Emma LAPREVOTE,
+            <h2 id="title_blogout"><span class="bw">H</span><span class="bw">e</span><span class="bw">l</span><span class="bw">l</span><span class="bw">o</span> <span class="bw">W</span><span class="bw">o</span><span class="bw">r</span><span class="bw">l</span><span class="bw">d</span><span class="bw">!</span></h2>
+            <p id="presentBlog">bLogOut est un petit blog créé par <strong>Nicolas REVEL</strong> et <strong>Emma LAPREVOTE</strong>,
                 tout deux étudiants au sein de l'école La Plateforme et passionnés par le développement web.
-            Ce blog est une vue généralisé de tout ce qui se passe dans l'univers du web, sur nos expériences tout au long de l'apprentissage.
+            Ce blog est une vue généralisé de tout ce qui se passe dans l'univers du web, sur nos expériences tout au long de l'apprentissage, afin de partager
+                notre vécu avec vous tous.
             </p>
         </div>
     </section>
 
+    <article id="LastArticle">
+        <h3 id="title_lastArticle">Derniers articles...</h3>
+        <p id="text_lastArticle">Les trois derniers articles mis en ligne, vous pouvez retrouvez l'intégralité des articles<a class= "buttonCard" href="articles.php" class="card-link">Here!</a></p>
+    </article>
+
 <div class="card_articles"><?= $show->showArticleAccueil(); ?></div>
 
-<a href="articles.php">Articles</a>
 </main>
+
+<?php require_once('../config/footer.php'); ?>
 <?php $pageContent = ob_get_clean(); ?>
 
 <?php require_once('template.php'); ?>
